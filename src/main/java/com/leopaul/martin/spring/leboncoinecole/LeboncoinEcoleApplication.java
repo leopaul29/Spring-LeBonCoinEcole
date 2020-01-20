669b4355980eeb94df5f2c443caecbb559dff978
leopaul29 <leopaulmartin@hotmail.fr>
2020-01-20 17:26:41 +0900

    first commit

---
+++ src/main/java/com/leopaul/martin/spring/leboncoinecole/LeboncoinEcoleApplication.java
@@ -0,0 +1,13 @@
+package com.leopaul.martin.spring.leboncoinecole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeboncoinEcoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeboncoinEcoleApplication.class, args);
+	}
+
+}
