524342c91d8d792ea5747b6e03d2dd8c4fdbd4ee
leopaul29 <leopaulmartin@hotmail.fr>
2020-03-29 00:31:27 +0900

    spring security permission to resources, login and logout pages

--- src/main/java/com/leopaulmartin/spring/leboncoinecole/config/SecurityConfig.java
+++ src/main/java/com/leopaulmartin/spring/leboncoinecole/config/SecurityConfig.java
@@ -1,56 +1,78 @@
 package com.leopaulmartin.spring.leboncoinecole.config;
 
 import com.leopaulmartin.spring.leboncoinecole.security.RestAccessDeniedHandler;
+import com.leopaulmartin.spring.leboncoinecole.services.UserService;
+import com.leopaulmartin.spring.leboncoinecole.web.controllers.MainController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
+	@Autowired
+	private UserService userService;
 	@Autowired
 	private RestAccessDeniedHandler accessDeniedHandler;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//				.authorizeRequests()
-//				.antMatchers(
-//						"/",
-//						"/js/**",
-//						"/css/**",
-//						"/img/**",
-//						"/webjars/**").permitAll()
-//				.antMatchers("/user/**").hasRole("USER")
-//				.anyRequest().authenticated()
-////				REST API
-////				.antMatchers("/admin/**").hasAnyRole("ROLE_ADMIN")
-////				.and()
-////				.exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+		http
+				.authorizeRequests()
+				.antMatchers(
+						"/", "/registration",
+						"/js/**",
+						"/css/**",
+						"/img/**",
+						"/webjars/**").permitAll()
+				.anyRequest().authenticated();
+//			.and()
+		http.formLogin()
+				.loginPage("/login")
+				.successForwardUrl("/")
+				.permitAll();
+//			.and()
+		http.logout()
+				.invalidateHttpSession(true)
+				.clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/login?logout")
+				.permitAll();
 //				.and()
-//				.formLogin()
-//				.loginPage("/login")
-//				.permitAll()
-//				.and()
-//				.logout()
-//				.invalidateHttpSession(true)
-//				.clearAuthentication(true)
-//				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//				.logoutSuccessUrl("/login?logout")
-//				.permitAll()
-//				.and()
-//				.exceptionHandling()
-//				.accessDeniedHandler(accessDeniedHandler);
-////				.and().csrf().disable();
+		http.exceptionHandling()
+				.accessDeniedHandler(accessDeniedHandler);
+//				.and().csrf().disable();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(userService);
+		auth.setPasswordEncoder(passwordEncoder());
+		return auth;
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication()
+		auth.authenticationProvider(authenticationProvider());
+//		auth
+//				.inMemoryAuthentication()
 //				.withUser("user").password("password").roles("USER")
 //				.and()
 //				.withUser("manager").password("password").roles("MANAGER");
