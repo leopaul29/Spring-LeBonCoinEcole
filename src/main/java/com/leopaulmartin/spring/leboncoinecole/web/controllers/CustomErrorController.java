77a39012ce6c28087b353868cd7b9b0055551e82
leopaul29 <leopaulmartin@hotmail.fr>
2020-03-31 02:51:00 +0900

    update error controller to handle properly the 503-access denied result

--- src/main/java/com/leopaulmartin/spring/leboncoinecole/web/controllers/CustomErrorController.java
+++ src/main/java/com/leopaulmartin/spring/leboncoinecole/web/controllers/CustomErrorController.java
@@ -1,11 +1,14 @@
-package com.leopaulmartin.spring.leboncoinecole.controllers;
+package com.leopaulmartin.spring.leboncoinecole.web.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +27,9 @@ public class CustomErrorController implements ErrorController {
 			if (statusCode == HttpStatus.NOT_FOUND.value()) {
 				logger.error("error/error-404");
 				return "error/error-404";
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				logger.error("error/access-denied");
+				return "error/access-denied";
 			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
 				logger.error("error/error-404");
 				return "error/error-500";
@@ -31,6 +37,15 @@ public class CustomErrorController implements ErrorController {
 		}
 		logger.error("error/my-error-page");
 		return "error/my-error-page";
+	}
+
+	@ExceptionHandler(Throwable.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public String exception(final Throwable throwable, final Model model) {
+		logger.error("Exception during execution of SpringSecurity application", throwable);
+		String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
+		model.addAttribute("errorMessage", errorMessage);
+		return "error";
 	}
 
 	@Override
